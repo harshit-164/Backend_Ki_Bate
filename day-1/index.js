823a4f61a7ef947c7cfada854382c57daf45a69e
harshit-164 <harshittrivedi164sharma@gmail.com>
2026-07-03 00:00:47 +0530

    intial bate

---
+++ day-1/index.js
@@ -0,0 +1,13 @@
+// console.log("Hello, World!");
+// console.log("Welcome to Day 1 of the backend coding.");
+
+
+
+// const bullet = require("cat-me");
+// console.log(bullet());
+
+
+
+const express = require("express");
+const app = express();   //server created
+app.listen(3000); //server is listening on port 3000 (started)
